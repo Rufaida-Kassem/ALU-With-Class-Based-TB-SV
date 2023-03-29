931b130f56d5b6f0c53f1a550acb61ea4c1cb7cc
rufaida-kassem <rufaidahkassem@gmail.com>
2023-03-30 01:25:40 +0200

    edit alu

--- src/ALU.sv
+++ src/ALU.sv
@@ -1,44 +1,38 @@
 // the ALU module
-// in1, in2: input data
-// sel: select the operation
+// a, b: input data
+// op: select the operation
 // out: output data
 package alu_pkg;
-localparam SELSIZE=4;
-typedef enum logic[SELSIZE-1:0] {NOP,ADD,SUB,MUL,DIV,SL,SR,AND,OR,NOT,XOR}
+localparam OP_WIDTH=4;
+typedef enum logic[OP_WIDTH-1:0] {NOP,ADD,SUB,MUL,DIV,SL,SR,AND,OR,NOT,XOR}
 opt_t;
 endpackage
 module alu
-#(
-parameter DATASIZE=8
-,localparam OUTSIZE = DATASIZE * 2
-)
+#( parameter DATA_WIDTH = 8,localparam OUT_WIDTH = DATA_WIDTH * 2)
 (
-input wire logic [DATASIZE-1:0]
-in1,
-input wire logic [DATASIZE-1:0]
-in2,
-input wire logic [alu_pkg::SELSIZE-1:0] sel,
-output wire logic [OUTSIZE-1:0]
+input wire logic [DATA_WIDTH-1:0]
+a,
+input wire logic [DATA_WIDTH-1:0]
+b,
+input wire logic [alu_pkg::OUT_WIDTH-1:0] op,
+output wire logic [OUT_WIDTH-1:0]
 out
 );
-
-// local variables
-
 
 // alu operations
 
 always_comb begin
-case (sel)
+case (op)
 alu_pkg::NOP: out = 0;  // no operation
-alu_pkg::ADD: out = in1 + in2;  // add
-alu_pkg::SUB: out = in1 - in2;  // subtract
-alu_pkg::MUL: out = in1 * in2;  // multiply
-alu_pkg::DIV: out = in1 / in2;  // divide
-alu_pkg::SL: out = in1 << 1;  // shift left
-alu_pkg::SR: out = in1 >> 1;  // shift right
-alu_pkg::AND: out = in1 & in2;  // and
-alu_pkg::OR: out = in1 | in2;   // or
-alu_pkg::NOT: out = ~in1;    // not
-alu_pkg::XOR: out = in1 ^ in2;  // xor
+alu_pkg::ADD: out = a + b;  // add
+alu_pkg::SUB: out = a - b;  // subtract
+alu_pkg::MUL: out = a * b;  // multiply
+alu_pkg::DIV: out = a / b;  // divide
+alu_pkg::SL: out = a << 1;  // shift left
+alu_pkg::SR: out = a >> 1;  // shift right
+alu_pkg::AND: out = a & b;  // and
+alu_pkg::OR: out = a | b;   // or
+alu_pkg::NOT: out = ~a;    // not
+alu_pkg::XOR: out = a ^ b;  // xor
 endcase
 end
